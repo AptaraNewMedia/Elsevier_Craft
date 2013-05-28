561e2856c74b98f2a4c61392130483eb75b06c3e
Rohit Yermalkar <Rohit.Yermalkar@aptaracorp.com>
2013-05-27 23:56:36 -0700

    radio view feedback issue resolved

--- PathoPhys/RadioGroupViewController.m
+++ PathoPhys/RadioGroupViewController.m
@@ -104,6 +104,8 @@
             myview.exclusiveTouch = YES;
         }
     }
+    
+    [scrollRadioOption setDelegate:self];
 }
 
 -(void) fn_SetFontColor
@@ -692,6 +694,7 @@
 //---------------------Delegate-------
 - (void)scrollViewDidScroll:(UIScrollView *)aScrollView
 {
+    NSLog(@"scrollview delegate method called.");
     // or if you are sure you wanna it always on left:
     [aScrollView setContentOffset: CGPointMake(0, aScrollView.contentOffset.y)];
     
